bd9cd86b6b78e775961b937d9b70e01709be3172
sheepnsleep-cell <sheepnsleep-cell@users.noreply.github.com>
2026-07-12 19:54:11 -0300

    v1.2.0: complete rewrite - minimal stable Facebook WebView client

--- app/src/main/java/com/freebook/app/FreebookApp.kt
+++ app/src/main/java/com/freebook/app/FreebookApp.kt
@@ -1,34 +1,5 @@
 package com.freebook.app
 
 import android.app.Application
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.os.Build
 
-class FreebookApp : Application() {
-
-    companion object {
-        const val NOTIFICATION_CHANNEL_ID = "freebook_notifications"
-        const val NOTIFICATION_CHANNEL_NAME = "Facebook Notifications"
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        createNotificationChannel()
-    }
-
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                NOTIFICATION_CHANNEL_ID,
-                NOTIFICATION_CHANNEL_NAME,
-                NotificationManager.IMPORTANCE_DEFAULT
-            ).apply {
-                description = "Notifications from Facebook"
-                enableVibration(true)
-            }
-            val manager = getSystemService(NotificationManager::class.java)
-            manager.createNotificationChannel(channel)
-        }
-    }
-}
+class FreebookApp : Application()
